02b32ea630e6bec4604cf53e3d2d1a41cde548d9
Antoine Gicquel <antoine.gicquel@mailoo.org>
2017-03-04 16:45:51 +0100

    ajout de la javadoc

---
+++ M2103/TP6/src/ScenarioBank.java
@@ -0,0 +1,10 @@
+package scenario;
+import bank.*;
+
+public class ScenarioBank
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ M2103/TP6/src/ScenarioCompte.java
@@ -0,0 +1,10 @@
+package scenario;
+import bank.*;
+
+public class ScenarioCompte
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
