1c0a4bf57b5cad506f9acec8bfdd6650ec02a1da
Antoine Gicquel <antoine.gicquel@mailoo.org>
2017-03-07 20:54:22 +0100

    finir Staff

--- M2103/TP7/src/Staff.java
+++ M2103/TP7/src/Staff.java
@@ -1,1 +1,50 @@
 package staff;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Staff
+{
+	ArrayList<StaffMember> staffList;
+
+	public Staff()
+	{
+		staffList = new ArrayList<StaffMember>();
+	}
+
+	public void addNewMember(StaffMember member)
+	{
+		if(member != null)
+		{
+			this.staffList.add(member);
+		}
+	}
+
+	public StaffMember getMember(int index)
+	{
+		StaffMember sm = null;
+		if(index <= this.staffList.size() && index >= 0)
+		{
+			sm = this.staffList.get(index);
+		}
+		return sm;
+	}
+
+	public void payday()
+	{
+		double paye = 0;
+		for(StaffMember sm : this.staffList)
+		{
+			System.out.println(sm);
+			paye = sm.pay();
+			if(paye == 0)
+			{
+				System.out.println("Thanks !");
+			}
+			else
+			{
+				System.out.println("Paid : " + paye);
+			}
+			System.out.println("-----------------------------------");
+		}
+	}
+}
