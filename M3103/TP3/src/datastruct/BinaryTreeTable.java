09d4e8ff36c301710d769a9eb8a22bc7dcb65e41
Antoine Gicquel <antoine.gicquel@mailoo.org>
2017-10-06 12:42:55 +0200

    avancement TP3

--- M3103/TP3/src/datastruct/BinaryTreeTable.java
+++ M3103/TP3/src/datastruct/BinaryTreeTable.java
@@ -12,20 +12,180 @@ public class BinaryTreeTable<E extends Comparable<E>, T> implements Table<E, T> 
 
     @Override
     public boolean insert(E key, T data) {
-        return false;
+        if(root == null) {
+            root = new Node(null, null, null, data, key);
+            return true;
+        }
+
+        boolean ret = false;
+        Node father = seekFather(key);
+        Node newNode = new Node(null, null, father, data, key);
+
+        if(father.lSon == null && father.key.compareTo(key) <= 0) {
+            father.lSon = newNode;
+            ret = true;
+        }
+        else if(father.rSon == null && father.key.compareTo(key) >= 0) {
+            father.rSon = newNode;
+            ret = true;
+        }
+
+        return ret;
     }
 
     @Override
     public boolean delete(E key) {
-        return false;
+        Node nodeToDel = findNode(root, key);
+        if(nodeToDel == null) {
+            return false;
+        }
+        delete(nodeToDel);
+        return true;
     }
 
     // BinaryTreeTable methods
 
     @Override
     public String toString() {
+        return getInfo(root);
+    }
+
+    /**
+     * Find the father for the given key
+     * @param key key of new node
+     * @return the father node
+     */
+    private Node seekFather(E key) {
+        Node father = root;
+        while(father.lSon != null && father.rSon != null) {
+            if(father.key.compareTo(key) <= 0) // father.key < key
+                father = father.lSon;
+            else if(father.key.compareTo(key) > 0) // father.key > key
+                father = father.rSon;
+        }
+
+        if(!(father.lSon == null && father.key.compareTo(key) <= 0) && !(father.rSon == null && father.key.compareTo(key) >= 0)) {
+            if(father.key.compareTo(key) <= 0)
+                father = father.lSon;
+            else if(father.key.compareTo(key) > 0)
+                father = father.rSon;
+        }
+
+        return father;
+    }
+
+    /**
+     * Recursive method to get info about the tree
+     * @param theN the root of the tree
+     * @return info found
+     */
+    private String getInfo(Node theN ) {
+        String infosLNode, infosRNode, infosNode;
         String ret = "";
+        if ( theN != null ) {
+            infosLNode = getInfo(theN.lSon);
+            infosRNode = getInfo(theN.rSon);
+            String infoFather = theN.father != null ? "Fclé=" + theN.father.key : "Fclé=n";
+            infosNode = "\n" + infoFather + "\t" + "clé=" + theN.key + "\tdata=" + theN.theValue;
+            ret = infosLNode + infosNode + infosRNode;
+        }
+        return ret ;
+    }
+
+    /**
+     * Find a specific node with the key
+     * @param theNode where the search begin
+     * @param key the given key
+     * @return the node if found
+     */
+    private Node findNode(Node theNode, E key) {
+        if(theNode == null) {
+            return null;
+        }
+        else if(theNode.key.compareTo(key) == 0) {
+            return theNode;
+        }
+        else {
+            if(theNode.key.compareTo(key) <= 0)
+                return findNode(theNode.lSon, key);
+            else if(theNode.key.compareTo(key) > 0)
+                return findNode(theNode.rSon, key);
+            else
+                return null;
+        }
+    }
+
+    /**
+     * Delete a specific node
+     * @param nodeToDel the node you want to delete
+     */
+    private void delete (Node nodeToDel) {
+        if(nodeToDel == null)
+            return;
+
+        if(nodeToDel.lSon == null && nodeToDel.rSon == null) {
+            if(nodeToDel.father.lSon == nodeToDel)
+                nodeToDel.father.lSon = null;
+            else if(nodeToDel.father.rSon == nodeToDel)
+                nodeToDel.father.rSon = null;
+
+            nodeToDel.father = null;
+        }
+        else if(nodeToDel.lSon != null && nodeToDel.rSon == null) {
+            nodeToDel.lSon.father = nodeToDel.father;
+            nodeToDel.father.lSon = nodeToDel.lSon;
+        }
+        else if(nodeToDel.lSon == null && nodeToDel.rSon != null ) {
+            nodeToDel.rSon.father = nodeToDel.father;
+            nodeToDel.father.lSon = nodeToDel.rSon;
+        }
+        else if(nodeToDel.lSon != null && nodeToDel.rSon != null) {
+            Node theGNode = null;
+            if(nodeToDel.lSon.key.compareTo(nodeToDel.rSon.key) >= 0)
+                theGNode = nodeToDel.lSon;
+            else
+                theGNode = nodeToDel.rSon;
+
+            nodeToDel.key = theGNode.key;
+            nodeToDel.theValue = theGNode.theValue;
+
+            delete(theGNode);
+        }
+    }
+
+    public String balanceLevel() {
+        int sizeL = sizeHeight(root.lSon, 0);
+        int sizeR = sizeHeight(root.rSon, 0);
+
+        String ret = "";
+        ret += "\nHauteur à gauche : " + sizeL;
+        ret += "\nHauteur à droite : " + sizeR;
+
         return ret;
+    }
+
+    private int sizeHeight(Node theNode, int currentSize) {
+        if(theNode == null)
+            return 0;
+
+        if(theNode.lSon == null && theNode.rSon == null) {
+            return currentSize + 1;
+        }
+        else if(theNode.lSon != null && theNode.rSon == null) {
+            return sizeHeight(theNode.lSon, currentSize + 1);
+        }
+        else if(theNode.lSon == null && theNode.rSon != null) {
+            return sizeHeight(theNode.rSon, currentSize + 1);
+        }
+        else {
+            int sizeL = sizeHeight(theNode.lSon, currentSize + 1);
+            int sizeR = sizeHeight(theNode.rSon, currentSize + 1);
+
+            if(sizeL > sizeR)
+                return sizeL + 1;
+            else
+                return sizeR + 1;
+        }
     }
 
     // Inner class Node
