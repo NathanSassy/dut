6e8792b585711b0e0042fc1eea9daac3b77e87eb
Antoine Gicquel <antoine.gicquel@mailoo.org>
2018-06-02 14:59:15 +0200

    ajout

--- M3105/TP1/td_code/ex3/ListeChainee.java
+++
@@ -1,9 +0,0 @@
-import java.util.Enumeration;
-public class
-ListeChainee
- {
-     private Noeud racine;
-     public Enumeration enumerate() {
-          return new ListeChaineeEnumeree (racine);
-     } // end enumerate
- }
--- M3105/TP1/td_code/ex3/Noeud.java
+++
@@ -1,8 +0,0 @@
-public class
-Noeud
- {
-   private Object contenu;
-   private Noeud suivant;
-   public Object contenu() { return contenu; }
-   public Noeud suivant() { return suivant; }
-} 
